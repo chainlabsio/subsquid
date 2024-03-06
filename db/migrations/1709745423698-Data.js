acc2b8ed1da80fd74ddb59a3476be33069d25ed6
Ant Somers <antsomers@gmail.com>
2024-03-06 20:17:25 +0300

    fix: make gasUsed nullable

--- db/migrations/1709745423698-Data.js
+++ db/migrations/1709745423698-Data.js
@@ -1,8 +1,8 @@
-module.exports = class Data1709743132919 {
-    name = 'Data1709743132919'
+module.exports = class Data1709745423698 {
+    name = 'Data1709745423698'
 
     async up(db) {
-        await db.query(`CREATE TABLE "transfer" ("id" character varying NOT NULL, "from" text NOT NULL, "to" text NOT NULL, "value" numeric NOT NULL, "gas_used" numeric NOT NULL, CONSTRAINT "PK_fd9ddbdd49a17afcbe014401295" PRIMARY KEY ("id"))`)
+        await db.query(`CREATE TABLE "transfer" ("id" character varying NOT NULL, "from" text NOT NULL, "to" text NOT NULL, "value" numeric NOT NULL, "gas_used" numeric, CONSTRAINT "PK_fd9ddbdd49a17afcbe014401295" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_be54ea276e0f665ffc38630fc0" ON "transfer" ("from") `)
         await db.query(`CREATE INDEX "IDX_4cbc37e8c3b47ded161f44c24f" ON "transfer" ("to") `)
     }
