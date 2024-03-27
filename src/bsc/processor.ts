6f3241f4c241a93a2bdedbf3975a59ce185696f9
Ant Somers <antsomers@gmail.com>
2024-03-27 21:26:45 +0300

    fix: binance gateway

--- src/bsc/processor.ts
+++ src/bsc/processor.ts
@@ -23,7 +23,7 @@ export const BSC_SHIB_ADDRESS =
 export const processor = new EvmBatchProcessor()
     // Lookup archive by the network name in Subsquid registry
     // See https://docs.subsquid.io/evm-indexing/supported-networks/
-    .setGateway(lookupArchive("eth-mainnet"))
+    .setGateway(lookupArchive("binance"))
     // Chain RPC endpoint is required for
     //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
     //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
